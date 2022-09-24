33ea157d6371d1f9bfab8dec3fd5beb4266d1a83
Guilherme <guipython2001@gmail.com>
2022-09-24 19:31:45 -0300

    add: new domain errors and fix build error [github]

--- domain/bet/errors/operation.go
+++ domain/bet/errors/operation.go
@@ -7,11 +7,12 @@ func (err *ErrInvalidBetOperation) Error() string {
 }
 
 var (
-	ErrCannotChangeName        = ErrInvalidBetOperation{"Name cannot be changed, because bet state must be suspended"}
-	ErrCannotChangeValue       = ErrInvalidBetOperation{"Value cannot be changed, because bet state must be suspended"}
-	ErrCannotChangeDescription = ErrInvalidBetOperation{"Description cannot be changed, because bet state must be suspended"}
-	ErrCannotRegisterGambler   = ErrInvalidBetOperation{"Cannot register a gambler because bet is closed"}
-	ErrCannotUnregisterGambler = ErrInvalidBetOperation{"Cannot unregister a gambler because bet is closed"}
-	ErrCannotDeposit           = ErrInvalidBetOperation{"Cannot deposit credits because bet is closed"}
-	ErrCannotWithdraw          = ErrInvalidBetOperation{"Cannot withdraw credits because bet is closed"}
+	ErrCannotChangeName                         = ErrInvalidBetOperation{"Name cannot be changed, because bet state must be suspended"}
+	ErrCannotChangeValue                        = ErrInvalidBetOperation{"Value cannot be changed, because bet state must be suspended"}
+	ErrCannotChangeDescription                  = ErrInvalidBetOperation{"Description cannot be changed, because bet state must be suspended"}
+	ErrCannotRegisterGamblerBetIsntOpen         = ErrInvalidBetOperation{"Cannot register a gambler because bet isn`t open"}
+	ErrCannotRegisterGamblerInsufficientCredits = ErrInvalidBetOperation{"Cannot register a gambler, insufficient credits for bet"}
+	ErrCannotUnregisterGamblerBetIsClosed       = ErrInvalidBetOperation{"Cannot unregister a gambler because bet isn`t open or suspended"}
+	ErrCannotDeposit                            = ErrInvalidBetOperation{"Cannot deposit credits because bet is closed"}
+	ErrCannotWithdraw                           = ErrInvalidBetOperation{"Cannot withdraw credits because bet is closed"}
 )
